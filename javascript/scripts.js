2cc21be03136ec5e801b1aad68a2e12d65e48b9d
Eduardo <DU123DUARDO@GMAIL.COM>
2026-02-25 12:26:04 -0300

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -12,6 +12,9 @@ function gerarAssinatura() {
     
     // 2. Construir o HTML da assinatura (Usando TABELA e Estilos INLINE)
     const htmlAssinatura = `
+                <p style="margin-bottom: 25px; padding-top: 0; color: #555; font-size: 12pt;">
+                     Atenciosamente,
+                </p>
         <table cellspacing="0" cellpadding="0" border="0" style="font-family: Arial, sans-serif; font-size: 10pt; line-height: 1.2;">
             <tr> 
                  <td style="border-right: 1px solid red; padding-right: 15px; vertical-align: top;">
@@ -24,61 +27,50 @@ function gerarAssinatura() {
                     </p>
                     
                     <div style="height: 5px; line-height: 5px; font-size: 1px;">&nbsp;</div>
+                    <p>
                     
-                    <p class="texto-padrao" style="margin: 1; padding-top: 20px; padding-bottom: 5px;">
-                        WhatsApp: <a href ="https://wa.me/55${whatsapp.replace(/\D/g, '')}"<span class="texto-complemento";">${whatsapp}</a>
-                    </p>
-
-                    <p class="texto-padrao" style="margin: 1;padding-top: 5px; padding-bottom: 20px;">
-                    
-                        <a>E-mail:</a> 
+                        <a class="texto-padrao">E-mail:</a> 
                         <a>
-                            <a href="mailto:${email}" class="texto-complemento";">${email}</a></b>
+                            <a href="mailto:${email}" class="texto-complemento";">${email}</a>
                         </a>
                     </p>
+                    <p>
+                        <a class="texto-padrao"> WhatsApp:</a> 
+                        <a href ="https://wa.me/55${whatsapp.replace(/\D/g, '')}"<span class="texto-complemento";">${whatsapp}</a>
+                    </p>
+
                     <!-- TABELA DUPLA AJUSTADA (CORREÇÃO PRINCIPAL) -->
-                    <table cellpadding="0" cellspacing="0" border="0" style="margin-top: 10px;">
+                    <table cellpadding="0" cellspacing="0" border="0" style="margin-top: 10px; font-family: Arial, sans-serif; border-collapse: collapse;">
                         <tr>
-                        <!-- COLUNA 1 -->
-                            <td style="vertical-align: top; padding-right: 20px;">
-                                <p style="margin: 0 0 3px 0;padding-bottom: 4px;text-align: center;">
-                                        <a class="texto-padrao"">Telefone Fixo:</a>
-
-                                        <p style="margin: 0 0 3px 0;">
-                                            <a href="tel:1121007400" class="texto-complemento";">
-                                                <u>(11) 2100-7400</u>
-                                            </a>
-                                        </p>
-
-                                    </a>
-                                </p>
+                            <td style="vertical-align: middle; padding-right: 20px; text-align: center;">
+                                <span class="texto-padrao" style="display: block; margin: 0; padding-bottom: 4px; font-size: 9pt;">
+                                    Telefone Fixo:
+                                </span>
+                                <a href="tel:1121007400" class="texto-complemento"style="text-decoration: underline;display: block; font-size: 9pt;">
+                                    (11) 2100-7400
+                                </a>
                             </td>
 
-                            <!-- COLUNA 2 -->
-                            <td style="vertical-align: top; padding-left: 20px; text-align: center; border-left: 1px solid #eee;">
-                                <p class="texto-padrao" style="margin: 0 0 3px 0;padding-bottom: 4px;text-align: center;">
+                            <td style="vertical-align: middle; padding-left: 20px; text-align: center; border-left: 1px solid #eeeeee;">
+                                <span class="texto-padrao"style="display: block; margin: 0; padding-bottom: 4px; font-size: 9pt;">
                                     Ramal:
-                                </p>
-                                <p class="texto-complemento" style="margin: 0 0 3px 0;">
+                                </span>
+                                <span class= "texto-complemento";style="display: block; font-size: 9pt;">
                                     ${ramal}
-                                </p>
+                                </span>
                             </td>
                         </tr>
                     </table>
-                </td>
-                
-                <p style="margin-bottom: 25px; padding-top: 0; color: #555; font-size: 12pt;">
-                     Atenciosamente,
-                </p>
                 
                 <td style="padding-left: 15px;">
                     <a href="https://www.verion.com.br/" target="_blank" style="text-decoration: none;"vertical-align: top;">
-                        <img src="${urlLogo}" alt="Logo da Verion" width="325" height="" style="display: block; border: none; padding-bottom: 20px;">
+                        <img src="${urlLogo}" alt="Logo da Verion" width="325" height="" style="display: block; border: none; margin-bottom: 30px;">
                     </a>
-                    <div style="padding-top: 20px; padding-bottom: 20px; margin:0; color: black; font-size: 10pt;">
+                    <div style=" margin:0; color: black; font-size: 10pt;">
                         <p class="texto-padrao" >
-                            Endereço:
-                             <div>
+                            
+                             <div class="texto-complemento">
+                                <a class="texto-padrao">Endereço:</a><br>
                                 <a href="https://maps.app.goo.gl/TcP9VjYx2XyAZge47" class="texto-complemento";">
                                 <u>R. Pedro de Toledo, 175 - Taboão, Guarulhos - SP, 07140-000</u>
                                 </a>
@@ -140,6 +132,34 @@ function copiarAssinatura() {
     
     alert('A assinatura foi SELECIONADA. Agora, pressione Ctrl+C (ou Cmd+C no Mac) para copiar e cole em Assinaturas no e-mail.');
 }
+// Máscara automática para telefone / WhatsApp
+function mascaraTelefone(input) {
+    let valor = input.value;
 
+    // Remove tudo que não for número
+    valor = valor.replace(/\D/g, '');
+
+    // Se não tiver número, deixa vazio
+    if (valor.length === 0) {
+        input.value = "";
+        return;
+    }
+
+    // Limita a 11 dígitos
+    valor = valor.substring(0, 11);
+
+    // Formatação dinâmica
+    if (valor.length > 10) {
+        valor = valor.replace(/^(\d{2})(\d{5})(\d{4}).*/, "($1) $2-$3");
+    } else if (valor.length > 6) {
+        valor = valor.replace(/^(\d{2})(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+    } else if (valor.length > 2) {
+        valor = valor.replace(/^(\d{2})(\d+)/, "($1) $2");
+    } else {
+        valor = "(" + valor;
+    }
+
+    input.value = valor;
+}
 // Chamar a função ao carregar a página para exibir os valores padrão
 window.onload = gerarAssinatura;
